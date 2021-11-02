fc7321c89e5772a3d68380d3071d0d89db057c3f
Damien4557 <goussindamien@gmail.com>
2021-11-02 12:36:32 +0100

    Modif class Vehicule ENUM

--- AutoVente/Models/Vehicule.cs
+++ AutoVente/Models/Vehicule.cs
@@ -7,9 +7,16 @@ namespace AutoVente.Models
 {
     public class Vehicule
     {
-        public int Id { get; set; }
-        public string Marque { get; set; }
+        public int Id_Vehicule { get; set; }
+        public DateTime Date_mise_en_service { get; set; }
+        public int Kilometrage { get; set; }
 
-        public string Description { get; set; }
+        public EtatVoiture Etat { get; set; }
     }
+}
+public enum EtatVoiture
+{
+    OCCASSION = 1,
+    BON = 2,
+    NEUF = 3
 }
