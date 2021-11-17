70b22e8ae008fd40c56bb9104b44caede979f5e1
joshua vadrot <joshuavadrot@gmail.com>
2021-11-17 17:53:44 +0100

    ajout AdresseService

---
+++ AutoVente/Service/AdresseService.cs
@@ -0,0 +1,23 @@
+﻿using AutoVente.DAO;
+using AutoVente.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoVente.Service
+{
+    public class AdresseService: BaseService<Adresse>
+    {
+        private SqlRepositoryAdresse daoAdresse;
+        public AdresseService()
+        {
+            daoAdresse = new SqlRepositoryAdresse(new MyContext());
+        }
+
+        public Adresse GetByIdUtilisateur(int idUtilisateur)
+        {
+            return daoAdresse.GetByIdUtilisateur(idUtilisateur);
+        }
+    }
+}
