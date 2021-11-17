2a8af1ec7452deae0c64287ec59b38b505a7781b
joshua vadrot <joshuavadrot@gmail.com>
2021-11-17 17:54:03 +0100

    Ajout CouleurService

---
+++ AutoVente/Service/CouleurService.cs
@@ -0,0 +1,23 @@
+﻿using AutoVente.DAO;
+using AutoVente.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoVente.Service
+{
+    public class CouleurService: BaseService<Couleur>
+    {
+        private SqlRepositoryCouleur daoCouleur;
+        public CouleurService()
+        {
+            daoCouleur = new SqlRepositoryCouleur(new MyContext());
+        }
+
+        public Couleur FindByCode(string code)
+        {
+            return daoCouleur.GetByCode(code);
+        }
+    }
+}
