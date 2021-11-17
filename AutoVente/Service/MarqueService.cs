f4592c70ca4d9a9bde833001f5276df68fe5fe09
Damien4557 <goussindamien@gmail.com>
2021-11-17 18:03:17 +0100

    MarqueService fini + UtilisateurService fini

---
+++ AutoVente/Service/MarqueService.cs
@@ -0,0 +1,26 @@
+﻿using AutoVente.DAO;
+using AutoVente.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace AutoVente.Service
+{
+    public class MarqueService : BaseServiceNom<Marque>
+    {
+        private SqlRepositoryMarque daoMarque;
+
+        public MarqueService()
+        {
+            daoMarque = new SqlRepositoryMarque(new MyContext());
+        }
+
+        public List<Vehicule> FindByOrigine(Origine origine)
+        {
+            return daoMarque.FindByOrigine(origine);
+        }
+
+    }
+}
---
+++ AutoVente/Service/UtilisateurService.cs
@@ -0,0 +1,27 @@
+﻿using AutoVente.DAO;
+using AutoVente.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoVente.Service
+{
+    public class UtilisateurService : BaseServiceNom<Utilisateur>
+    {
+        private SqlRepositoryUtilisateur daoUtilisateur;
+        public UtilisateurService()
+        {
+            daoUtilisateur = new SqlRepositoryUtilisateur(new MyContext());
+        }
+
+        public Utilisateur GetByEmail(string email)
+        {
+            return daoUtilisateur.GetByEmail(email);
+        }
+        public Utilisateur GetByTelephone(string telephone)
+        {
+            return daoUtilisateur.GetByTelephone(telephone);
+        }
+    }
+}
