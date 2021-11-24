8b43bbebed304c060adbe26c299300afc0e48440
Ben Gamra <drogba_filsdelune@hotmail.com>
2021-11-24 17:49:23 +0100

    Add Utilisateur et Marque

--- AutoVente/Service/MarqueService.cs
+++ AutoVente/Service/MarqueService.cs
@@ -5,22 +5,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-
 namespace AutoVente.Service
 {
     public class MarqueService : BaseServiceNom<Marque>
     {
-        private SqlRepositoryMarque daoMarque;
+     
+            private SqlRepositoryMarque daoMarque;
 
-        public MarqueService()
-        {
-            daoMarque = new SqlRepositoryMarque(new MyContext());
-        }
+            public MarqueService()
+            {
+                daoMarque = new SqlRepositoryMarque(new MyContext());
+            }
 
-        public List<Vehicule> FindByOrigine(Origine origine)
-        {
-            return daoMarque.FindByOrigine(origine);
-        }
-
-    }
+            public List<Vehicule> FindByOrigine(Origine origine)
+            {
+                return daoMarque.FindByOrigine(origine);
+            }
+        
 }
