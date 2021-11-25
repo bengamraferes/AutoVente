863a3577dbd6409d0b4d9510344221d5fd2b01c8
Damien4557 <goussindamien@gmail.com>
2021-11-25 17:12:31 +0100

    Ajout Classe Dam

---
+++ AutoVente/Models/Dam.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoVente.Models
+{
+    public class Dam
+    {
+        public string Name { get; set; }
+    }
+}
