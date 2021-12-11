0406d71c4455a9eb76d93d5466ec835abd5eb14c
Ben Gamra <drogba_filsdelune@hotmail.com>
2021-12-11 23:56:15 +0100

    Model edit --

--- AutoVente/Controllers/ModelController.cs
+++ AutoVente/Controllers/ModelController.cs
@@ -1,5 +1,7 @@
-﻿using AutoVente.Models;
+﻿using AutoVente.Extensions;
+using AutoVente.Models;
 using AutoVente.Service;
+using AutoVente.ViewsModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,75 @@ namespace AutoVente.Controllers
         {
             List<Model> models = service.GetAll().OrderBy(m => m.Nom).ToList();
             return View(models);
+        }
+        public ActionResult Edit(int id)
+        {
+            Model model = service.FindById(id);
+            if (model != null)
+            {
+                TempData["EditModel"] = "EditModel";
+                TempData["IdModel"] = model.Id;
+                TempData.Keep();
+            }
+            return RedirectToAction("index");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Model model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                service.Update(model);
+                service.SaveChanges();
+                this.AddNotification("La Model " + model.Nom + " a été modifiée", NotificationType.SUCCESS);
+            }
+            else
+            {
+                TempData["EditModel"] = "EditModel";
+                TempData["IdModel"] = model.Id;
+                TempData.Keep();
+                this.AddNotification("Echec de mise à jour de la Model " + model.Nom, NotificationType.ERROR);
+            }
+            return RedirectToAction("index");
+        }
+        public ActionResult Create()
+        {
+
+            TempData["CreateModel"] = "CreateModel";
+            TempData.Keep();
+
+            return RedirectToAction("index");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ModelMarqueViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Model model = viewModel.Model;
+                //model.Marque = model.
+                service.Insert(model);
+                service.SaveChanges();
+                this.AddNotification("Creation de la" + model.Nom, NotificationType.SUCCESS);
+                TempData["NomModel"] = model.Nom.ToString();
+                TempData.Keep();
+            }
+            else
+            {
+                this.AddNotification("Echec de creation", NotificationType.WARNING);
+                TempData["CreateModel"] = "CreateModel";
+                TempData.Keep();
+            }
+            return RedirectToAction("index");
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id)
+        {
+            service.Delete(id);
+            service.SaveChanges();
+            return RedirectToAction("index");
         }
     }
 }
---
+++ AutoVente/ViewsModels/ModelMarqueViewModel.cs
@@ -0,0 +1,23 @@
+﻿using AutoVente.Models;
+using AutoVente.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoVente.ViewsModels
+{
+    public class ModelMarqueViewModel
+    {
+        public Model Model { get; set; }
+        public List<Marque> Marques { get; set; }
+        public Marque Marque { get; set; }
+
+        private MarqueService service;
+        public ModelMarqueViewModel()
+        {
+            service = new MarqueService();
+            Marques = service.GetAll().ToList();
+        }
+    }
+}
