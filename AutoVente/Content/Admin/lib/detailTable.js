678e3cfe193a3048802832f06b0e1a753cf76d96
Ben Gamra <drogba_filsdelune@hotmail.com>
2021-12-14 23:58:19 +0100

    fix model

--- AutoVente/Content/Admin/lib/detailTable.js
+++ AutoVente/Content/Admin/lib/detailTable.js
@@ -1,26 +1,28 @@
 ﻿let detailBtns = document.querySelectorAll(".detail-btn");
+let actives = document.querySelectorAll(".active");
+console.log(actives)
+actives.forEach((e) => {
+    e.classList.remove("hidden")
+
+})
 detailBtns.forEach((e) => {
     let i = e.querySelector("i");
+
     e.addEventListener("click", ec => {
+      
         let idEc = e.getAttribute('id');
         let id = idEc.slice(11);
         let tr = document.querySelector("#detail-tr-" + id)
-
-        tr.classList.add("active");
+        tr.classList.add("active"); 
+     
         i.classList.toggle("fa-plus-circle")
         i.classList.toggle("fa-minus-circle")
         tr.classList.toggle("hidden")
+      
 
     })
-    if (tr.classList.contains("active" + id) && tr.classList.contains("hidden")) {
-        tr.classList.remove("hidden")
-        i.classList.toggle("fa-minus-circle")
-    }
    
 })
-window.onload = function () {
-    let actives = document.querySelectorAll(".active");
-    actives.forEach((e) => {
 
-    })
-}
+          
+  
