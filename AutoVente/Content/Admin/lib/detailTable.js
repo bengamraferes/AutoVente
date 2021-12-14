986abed8fad6230a34d3af0794e24b86078fef97
Ben Gamra <drogba_filsdelune@hotmail.com>
2021-12-14 17:23:21 +0100

    affichage model

--- AutoVente/Content/Admin/lib/detailTable.js
+++ AutoVente/Content/Admin/lib/detailTable.js
@@ -1,12 +1,26 @@
 ﻿let detailBtns = document.querySelectorAll(".detail-btn");
 detailBtns.forEach((e) => {
+    let i = e.querySelector("i");
     e.addEventListener("click", ec => {
         let idEc = e.getAttribute('id');
         let id = idEc.slice(11);
         let tr = document.querySelector("#detail-tr-" + id)
-        let i = e.querySelector("i");
+
+        tr.classList.add("active");
         i.classList.toggle("fa-plus-circle")
         i.classList.toggle("fa-minus-circle")
         tr.classList.toggle("hidden")
+
     })
+    if (tr.classList.contains("active" + id) && tr.classList.contains("hidden")) {
+        tr.classList.remove("hidden")
+        i.classList.toggle("fa-minus-circle")
+    }
+   
 })
+window.onload = function () {
+    let actives = document.querySelectorAll(".active");
+    actives.forEach((e) => {
+
+    })
+}
