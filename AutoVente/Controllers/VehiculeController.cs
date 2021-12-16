b1d6a5c3ac837bf4ac4d1791437d21a3b22681b8
joshua vadrot <joshuavadrot@gmail.com>
2021-12-16 10:19:56 +0100

    vehicules en cours de creation

---
+++ AutoVente/Controllers/VehiculeController.cs
@@ -0,0 +1,36 @@
+﻿using AutoVente.Models;
+using AutoVente.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoVente.Controllers
+{
+    public class VehiculeController : Controller
+    {
+        private VehiculeService service;
+
+        public VehiculeController()
+        {
+            service = new VehiculeService();
+        }
+
+        // GET: Voiture
+        public ActionResult Index()
+        {
+            List<Vehicule> vehicules = service.GetAll().OrderBy(v => v.Model.Marque.Nom).ToList();
+            return View(vehicules);
+        }
+
+        public ActionResult Create()
+        {
+
+            TempData["CreateVehicule"] = "CreateVehicule";
+            TempData.Keep();
+
+            return RedirectToAction("index");
+        }
+    }
+}
