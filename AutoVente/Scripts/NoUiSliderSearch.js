ad8881f1848bf934f82259bad8801692d0ce811b
Ben Gamra <drogba_filsdelune@hotmail.com>
2021-12-22 16:10:54 +0100

    Ui slider suite

--- AutoVente/Scripts/NoUiSliderSearch.js
+++ AutoVente/Scripts/NoUiSliderSearch.js
@@ -3,14 +3,14 @@
 noUiSlider.create(nonLinearSlider, {
     connect: true,
     behaviour: 'tap',
-    start: [10, 40],
+    start: [5000,10000],
     range: {
         // Starting at 500, step the value by 500,
         // until 4000 is reached. From there, step by 1000.
-        'min': [0],
-        '10%': [5, 5],
-        '50%': [50, 100],
-        'max': [100]
+        'min': [2000],
+        '10%': [100, 100],
+        '50%': [25000, 100],
+        'max': [50000]
     }
 });
 var inputNumber = document.getElementById('input-number');
@@ -20,7 +20,7 @@ nonLinearSlider.noUiSlider.on('update', function (values, handle) {
     var value = values[handle];
 
     if (handle) {
-        inputNumber.value = value;
+        inputNumber.value = Math.round(value);
 
     } else {
         inputNumberMin.value = Math.round(value);
@@ -33,4 +33,78 @@ inputNumberMin.addEventListener('change', function () {
 
 inputNumber.addEventListener('change', function () {
     nonLinearSlider.noUiSlider.set([null, this.value]);
+});
+//////////////////////////////////////////// Annee//////////
+var nonLinearSliderAnnee = document.getElementById('nonlinearAnnee');
+
+noUiSlider.create(nonLinearSliderAnnee, {
+    connect: true,
+    behaviour: 'tap',
+    start: [2005, 2018],
+    range: {
+        // Starting at 500, step the value by 500,
+        // until 4000 is reached. From there, step by 1000.
+        'min': [1930],
+        '10%': [1, 1],
+        '50%': [2005, 1],
+        'max': [2022]
+    }
+});
+var inputNumberAnnee = document.getElementById('input-numberAnnee');
+var inputNumberMinAnnee = document.getElementById('input-numberMinAnnee');
+nonLinearSliderAnnee.noUiSlider.on('update', function (values, handle) {
+
+    var value = values[handle];
+
+    if (handle) {
+        inputNumberAnnee.value = Math.round(value);
+
+    } else {
+        inputNumberMinAnnee.value = Math.round(value);
+    }
+});
+
+inputNumberMinAnnee.addEventListener('change', function () {
+    nonLinearSliderAnnee.noUiSlider.set([this.value, null]);
+});
+
+inputNumberAnnee.addEventListener('change', function () {
+    nonLinearSliderAnnee.noUiSlider.set([null, this.value]);
+});
+//////////////////////////////////////////// Kilometrrage//////////
+var nonLinearSliderKilometrrage = document.getElementById('nonlinearKilometrage');
+
+noUiSlider.create(nonLinearSliderKilometrrage, {
+    connect: true,
+    behaviour: 'tap',
+    start: [10000, 50000],
+    range: {
+        // Starting at 500, step the value by 500,
+        // until 4000 is reached. From there, step by 1000.
+        'min': [0],
+        '10%': [1000, 1000],
+        '50%': [125000, 1000],
+        'max': [250000]
+    }
+});
+var inputNumberKilometrrage = document.getElementById('input-numberKilometrrage');
+var inputNumberMinKilometrrage = document.getElementById('input-numberMinKilometrrage');
+nonLinearSliderKilometrrage.noUiSlider.on('update', function (values, handle) {
+
+    var value = values[handle];
+
+    if (handle) {
+        inputNumberKilometrrage.value = Math.round(value);
+
+    } else {
+        inputNumberMinKilometrrage.value = Math.round(value);
+    }
+});
+
+inputNumberMinKilometrrage.addEventListener('change', function () {
+    nonLinearSliderKilometrrage.noUiSlider.set([this.value, null]);
+});
+
+inputNumberKilometrrage.addEventListener('change', function () {
+    nonLinearSliderKilometrrage.noUiSlider.set([null, this.value]);
 });
